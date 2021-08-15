72df5c06ea72efd881d67f1881c1bec5547faa31
Crowdbotics <team@crowdbotics.com>
2021-08-15 14:47:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "stay_here_pay_here_29825Identifier",
+  url: "https://stay_here_pay_here_29825Identifier.botics.co",
+  api: "https://stay_here_pay_here_29825Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
